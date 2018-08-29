f5944be469df3e08d2afa66c938f7c3dc70cc277
MrMessage <mr.me.sss.age@gmail.com>
2018-08-29 22:11:54 +0300

    Add modification Category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: webdev
- * Date: 29.08.2018
- * Time: 21:51
- */
+@extends('admin.layouts.admin_app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                @component('admin.components.breadcrumbs')
+                    @slot('title') Категории @endslot
+                    @slot('parent') Административная панель @endslot
+                    @slot('active') Список категорий @endslot
+                @endcomponent
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <table class="table text-center">
+                    <thead>
+                    <th>#</th>
+                    <th>Заголовок</th>
+                    <th>Редактировать</th>
+                    </thead>
+                    <tbody>
+                    @forelse( $categories as $category)
+                        <tr>
+                            <td>{{$category->id}}</td>
+                            <td><a href="{{Route('admin.catogory.show', $category->id)}}">{{$category->title}}</a></td>
+                            <td><a href="{{Route('admin.catogory.edit', $category->id)}}"><i class="fa fa-edit"></i></a></td>
+                        </tr>
+                    @empty
+                        <tbody><tr><td colspan="3">Данные не найдены</td></tr></tbody>
+                    @endforelse
+                    </tbody>
+                    <tfooter>
+                        <tr><td colspan="3">{{$categories->links()}}</td></tr>
+                    </tfooter>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- resources/views/admin/layouts/admin_app.blade.php
+++ resources/views/admin/layouts/admin_app.blade.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -37,7 +38,7 @@
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Блог</a>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Категории</a>
+                                <a class="dropdown-item" href="{{ Route('admin.category.index') }}">Категории</a>
                                 <a class="dropdown-item" href="#">Материалы</a>
                             </div>
                         </li>
