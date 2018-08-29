2b754c0f43b868ad7959fe2f162f680058bbd7ff
MrMessage <mr.me.sss.age@gmail.com>
2018-08-29 22:11:16 +0300

    Create controll/model/view for Category (index)

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webdev
+ * Date: 29.08.2018
+ * Time: 21:51
+ */
---
+++ resources/views/admin/components/breadcrumbs.blade.php
@@ -0,0 +1,7 @@
+<h1>{{title}}</h1>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ Route('admin.index')}}">{{parent}}</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{active}}</li>
+    </ol>
+</nav>
